43aa54ec14320eb9756b34a75fc85cd531d84007
Jacob Moya <49262410+jacobEliasMoya@users.noreply.github.com>
2025-04-07 15:33:56 -0700

    Update functions.php
    update to functions.php

--- functions.php
+++ functions.php
@@ -149,41 +149,53 @@ function breakout_iframe_shortcode($atts, $content = null) {
 
 add_shortcode('breakout_iframe', 'breakout_iframe_shortcode');
 
-function breakout_slider_gn_shortcode($atts, $content = null) {
-	$atts = shortcode_atts([
-		'image_src'   => '',
-		'header'      => '',
-		'sub_header'  => '',
-	], $atts);
+ function breakout_slider_shortcode($atts, $content = null) {
+        ob_start();
+    ?>
+    <div class="breakout p-0">
+        <section class="slider-wrapper">
+            <ul class="slides-container d-flex" id="slides-container">
+                <?php echo do_shortcode($content); ?>
+            </ul>
+        </section>
+    </div>
+    <?php 
+        return ob_get_clean();
+    }
+    add_shortcode('breakout_slider', 'breakout_slider_shortcode');
 
-	ob_start();
-?>
-<div class="breakout p-0">
-	<section class="slider-wrapper">
-		<ul class="slides-container" id="slides-container">
-			<li class="slide" style="background-image: url('<?php echo esc_url($atts['image_src']); ?>')">
-				<div class="slider-content d-flex justify-content-center">
-					<?php if (!empty($atts['sub_header'])): ?>
-					<div class="slide-sub-header"><?php echo esc_html($atts['sub_header']); ?></div>
-					<?php endif; ?>
+    function breakout_slide_shortcode($atts, $content = null) {
+        $atts = shortcode_atts([
+            'image_src'   => '',
+            'header'      => '',
+            'sub_header'  => '',
+        ], $atts);
 
-					<?php if (!empty($atts['header'])): ?>
-					<div class="slide-header"><?php echo esc_html($atts['header']); ?></div>
-					<?php endif; ?>
+        ob_start();
+    ?>
+    <li class="slide " style="background-image: url('<?php echo esc_url($atts['image_src']); ?>')">
+        <div class="slider-content d-flex justify-content-center">
 
-					<?php if (!empty($content)): ?>
-					<div class="all-the-ctas"><?php echo do_shortcode(wpautop($content)); ?></div>
-					<?php endif; ?>
-				</div>
-			</li>
-		</ul>
-	</section>
-</div>
-<?php
-	return ob_get_clean();
-}
+            <?php if (!empty($atts['header'])): ?>
+                <div class="slide-sub-header"><?php echo esc_html($atts['header']); ?></div>
+            <?php endif; ?>
 
-add_shortcode('breakout_slider_gn', 'breakout_slider_gn_shortcode');
+            <?php if (!empty($atts['sub_header'])): ?>
+                <div class="slide-header"><?php echo esc_html($atts['sub_header']); ?></div>
+            <?php endif; ?>
+
+            <?php if (!empty($content)): ?>
+            <div class="all-the-ctas">
+                <?php echo do_shortcode(wpautop($content)); ?>
+            </div>
+            <?php endif; ?>
+
+        </div>
+    </li>
+    <?php
+        return ob_get_clean();
+    }
+    add_shortcode('breakout_slide', 'breakout_slide_shortcode');
 
 function breakout_image_half($atts, $content = null) {
 	$atts = shortcode_atts([
